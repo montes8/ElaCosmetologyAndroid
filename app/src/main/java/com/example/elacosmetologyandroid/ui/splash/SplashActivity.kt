083af5ee2b7312c9fdbd0f392fea6b145cddda3a
tayler <eymontesinos@indracompany.com>
2021-11-20 23:29:24 -0500

    agregando iconos laucher

--- app/src/main/java/com/example/elacosmetologyandroid/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/elacosmetologyandroid/ui/splash/SplashActivity.kt
@@ -1,11 +1,15 @@
 package com.example.elacosmetologyandroid.ui.splash
 
 
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
 import com.example.elacosmetologyandroid.R
 import com.example.elacosmetologyandroid.ui.BaseActivity
 import com.example.elacosmetologyandroid.databinding.ActivitySplashBinding
+import com.example.elacosmetologyandroid.ui.login.LoginActivity
 
 class SplashActivity : BaseActivity() {
 
@@ -17,15 +21,25 @@ class SplashActivity : BaseActivity() {
         binding.lifecycleOwner = this
     }
 
-
-
     override fun setUpView() {
-
+        configAnimation()
     }
 
-    override fun observeLiveData() {
-
+    private fun configAnimation(){
+        val ani = AnimationUtils.loadAnimation(this, R.anim.animation_top)
+        val ani2 = AnimationUtils.loadAnimation(this, R.anim.animation_botton)
+        binding.ctlTop.animation=ani
+        binding.ctlBottom.animation=ani2
+        initSplash()
     }
+
+    private fun initSplash(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            LoginActivity.start(this)
+        }, 4500)
+    }
+
+    override fun observeLiveData() {}
 
     override fun getErrorObservers(): ArrayList<MutableLiveData<Throwable>> = arrayListOf()
 
