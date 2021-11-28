3c99fcd7c2b11a45b426be2f4c0604e96d495a7f
tayler <eymontesinos@indracompany.com>
2021-11-27 19:30:53 -0500

    mapeando fonto general

--- app/src/main/java/com/example/elacosmetologyandroid/utils/Constans.kt
+++ app/src/main/java/com/example/elacosmetologyandroid/utils/Constans.kt
@@ -3,6 +3,7 @@ package com.example.elacosmetologyandroid.utils
 const val EMPTY = ""
 const val CONFIG_ITEM = "item.json"
 const val USER_ROLE = "USER_ROLE"
+const val ADMIN_ROLE = "ADMIN_ROLE"
 
 
 const val TITLE_DIALOG_DEFAULT = "Gabbi \nCosmetology"
